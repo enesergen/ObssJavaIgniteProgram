00f12605571aeec07eb95b166eaa380abb3e8395
Enes Ergen <eenes44@gmail.com>
2022-07-27 10:41:05 +0300

    servlet path functions and http status code using

---
+++ ServletFunctionsExample/src/main/java/com/example/servletfunctionsexample/HelloServlet.java
@@ -0,0 +1,35 @@
+package com.example.servletfunctionsexample;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet/*")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        if(request.getRequestURL().toString().contains("secured")){
+            response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        }else {
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            out.println("<h1>" + "Request URL:"+request.getRequestURL() + "</h1>");
+            out.println("<h1>" + "getRequestURI:"+request.getRequestURI() + "</h1>");
+            out.println("<h1>" + "getContextPath:"+request.getContextPath() + "</h1>");
+            out.println("<h1>" + "getPathInfo:"+request.getPathInfo() + "</h1>");
+            out.println("<h1>" + "getPathTranslated:"+request.getPathTranslated() + "</h1>");
+            out.println("</body></html>");
+        }
+
+
+    }
+
+    public void destroy() {
+    }
+}
