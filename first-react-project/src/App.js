855c280baad031a9f1e33b90c17f6ee3a30558f6
Enes Ergen <eenes44@gmail.com>
2022-08-16 20:33:04 +0300

    react starter mui templates example

---
+++ first-react-project/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Login from "./Login";
+import Register from "./Register"
+export default function App() {
+    return (
+        <Login />
+    );
+}
+//deneme 1 2 3
